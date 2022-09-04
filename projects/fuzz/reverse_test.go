23ed50abb2e5f1d64184318b902aa4d4683b8292
Li Fei <hefrankeleyn@gamil.com>
2022-09-04 13:05:53 +0800

    模糊测试：打印Debugger

--- projects/fuzz/reverse_test.go
+++ projects/fuzz/reverse_test.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		{"!12345", "54321!"},
+func FuzzReverse(f *testing.F) {
+	testcase := []string{"Hello World", " ", "!12345"}
+	for _, tc := range testcase {
+		f.Add(tc)
 	}
-	for _, tc := range testcases {
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
 		}
-	}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
