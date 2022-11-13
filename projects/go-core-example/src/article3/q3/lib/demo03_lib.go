46663e93a49d0a33f82bf4940f81f022e245bca0
Li Fei <hefrankeleyn@gamil.com>
2022-11-13 11:52:33 +0800

    导入包，最后一级名称相同而导致的冲突

--- projects/go-core-example/src/article3/q3/lib/demo03_lib.go
+++ projects/go-core-example/src/article3/q3/lib/demo03_lib.go
@@ -1,11 +1,10 @@
-// package lib2 // 第一个改动， 把 main 改为 lib2。（故意让声明的包名和所在的目录名称不同）
 package lib // 为了不让代码包的使用者产生困惑，我们总是应该让声明的包名与其父目录的名称一致
 
 import (
-	"fmt"
+	in "article3/q3/lib/internal"
+	"os"
 )
 
-// 第二个改动， 把首字母小写的 hello 改为 首字母大写的 Hello
 func Hello(name string) {
-	fmt.Println(name)
+	in.Hello(os.Stdout, name)
 }
